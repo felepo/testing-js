09f44b043d961aa83a0d9ca325340bd7a3150e0f
felepo <fecrackle@gmail.com>
2025-02-13 01:13:35 -0600

    Implementing integration test conf with supertest

---
+++ api/e2e/hello.e2e.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+
+const createApp = require('../src/app');
+
+describe('Test for hello endpoint', () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('Test for [GET] /', () => {
+    // test('Should return "Hello World!"', () => request(app)
+    //   .get('/')
+    //   .expect(200)
+    //   .then((response) => {
+    //     expect(response.text).toEqual('Hello World!');
+    //   }));
+
+    // Using async/await functions
+    test('Should return "Hello World!"', async () => {
+      const response = await request(app).get('/');
+      expect(response.text).toEqual('Hello World!');
+    });
+  });
+});
